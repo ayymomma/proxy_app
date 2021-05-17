243f287f1aaed5e90e8fc546261732ff87ab4f7c
Mircea Mihai Adrian <62558071+ayymomma@users.noreply.github.com>
2021-05-17 22:35:42 +0300

    Update ProxyDocumentManager.cs

--- ProxyDocumentManager.cs
+++ ProxyDocumentManager.cs
@@ -1,19 +1,4 @@
-/**************************************************************************
- *                                                                        *
- *  File:        DocumentManager.cs                                       *
- *  Copyright:   (c) 2008-2019, Florin Leon                               *
- *  E-mail:      florin.leon@tuiasi.ro                                    *
- *  Website:     http://florinleon.byethost24.com/lab_ip.htm              *
- *  Description: Secret documents application with Protection Proxy.      *
- *               The Protection Proxy (Software Engineering lab 9)        *
- *                                                                        *
- *  This code and information is provided "as is" without warranty of     *
- *  any kind, either expressed or implied, including but not limited      *
- *  to the implied warranties of merchantability or fitness for a         *
- *  particular purpose. You are free to use this source code in your      *
- *  applications as long as the original copyright notice is included.    *
- *                                                                        *
- **************************************************************************/
+
 
 using System;
 using System.Collections.Generic;
